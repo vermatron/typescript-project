967e85a092b487e302fd319fd051dc7e2cfdbcb5
Vermatron <contact.ashitverma@gmail.com>
2023-11-28 19:40:25 +0000

    Playing with tuple

--- app.ts
+++ app.ts
@@ -1,1 +1,17 @@
 console.log("Your code goes hssssfffere...");
+
+const person: {
+  name: string;
+  age: number;
+  sports: string[];
+  role: [number, string];
+} = {
+  name: "Ashit",
+  age: 30,
+  sports: ["cricket", "hockey"],
+  role: [2, "admin"],
+};
+// person.role.push("someone");
+// person.role[1] = 2;
+
+console.log(person);
